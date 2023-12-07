957cbd96a4719cf558307f25fce60118330d7c07
CantarinoG <guilhermecantarino1709@gmail.com>
2023-12-07 08:20:09 -0300

    Add transactionList as a separate component

--- lib/main.dart
+++ lib/main.dart
@@ -1,6 +1,6 @@
+import 'package:expenses/components/transactionList.dart';
 import 'package:expenses/models/transaction.dart';
 import 'package:flutter/material.dart';
-import 'package:intl/intl.dart';
 
 void main() {
   runApp(const ExpensesApp());
@@ -18,6 +18,9 @@ class ExpensesApp extends StatelessWidget {
 
 class HomePage extends StatelessWidget {
   HomePage({super.key});
+
+  final titleController = TextEditingController();
+  final valueController = TextEditingController();
 
   final _transactions = [
     Transaction(id: "0", title: "Luz", value: 147.38, date: DateTime.now()),
@@ -38,44 +41,7 @@ class HomePage extends StatelessWidget {
             child: Card(child: Text("Gráfico")),
             width: double.infinity,
           ),
-          Column(
-            children: _transactions.map((e) {
-              return Card(
-                child: Row(
-                  children: <Widget>[
-                    Container(
-                      margin:
-                          EdgeInsets.symmetric(horizontal: 15, vertical: 10),
-                      decoration: BoxDecoration(
-                          border: Border.all(color: Colors.purple, width: 2)),
-                      padding: EdgeInsets.all(10),
-                      child: Text(
-                        "R\$" + e.value.toStringAsFixed(2),
-                        style: TextStyle(
-                            fontWeight: FontWeight.bold,
-                            fontSize: 20,
-                            color: Colors.purple),
-                      ),
-                    ),
-                    Column(
-                      crossAxisAlignment: CrossAxisAlignment.start,
-                      children: <Widget>[
-                        Text(
-                          e.title,
-                          style: TextStyle(
-                              fontSize: 16, fontWeight: FontWeight.bold),
-                        ),
-                        Text(
-                          DateFormat('d MMM y').format(e.date),
-                          style: TextStyle(color: Colors.grey),
-                        )
-                      ],
-                    )
-                  ],
-                ),
-              );
-            }).toList(),
-          ),
+          TransactionList(transactions: _transactions),
           Card(
             elevation: 5,
             child: Padding(
@@ -83,16 +49,23 @@ class HomePage extends StatelessWidget {
               child: Column(
                 children: <Widget>[
                   TextField(
+                    controller: titleController,
                     decoration: InputDecoration(labelText: 'Título'),
                   ),
                   TextField(
+                    controller: valueController,
                     decoration: InputDecoration(labelText: "Valor (R\$)"),
                   ),
                   Row(
                     mainAxisAlignment: MainAxisAlignment.end,
                     children: <Widget>[
                       MaterialButton(
-                        onPressed: () {},
+                        onPressed: () {
+                          print(titleController.text);
+                          print(valueController.text);
+                          titleController.text = "";
+                          valueController.text = "";
+                        },
                         child: Text(
                           "Nova Transação",
                           style: TextStyle(
